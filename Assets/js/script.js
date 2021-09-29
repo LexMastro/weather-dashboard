81b0e59592e1e4bf655d0779104bc5d69b9cbeef
Alexis Mastrodomenico <86946841+LexMastro@users.noreply.github.com>
2021-09-29 12:28:56 +1000

    trying to De Bug

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,22 @@
+function convertUnix(unix_timestamp) {
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    let date = new Date(unix_timestamp * 1000);
+    // Hours part from the timestamp
+    let year = date.getFullYear();
+    // Minutes part from the timestamp
+    let months = date.getMonth() + 1;
+    // Seconds part from the timestamp
+    let days = date.getDate();
+
+    // Will display time in 10:30:23 format
+    return year + '/' + months + '/' + days;
+
+}
+
+
+let searchHistory = [];
+let cityName = $("#cityname").val();
 
 // Begin search by name input by user
 $(document).ready(function () {
@@ -5,82 +24,95 @@ $(document).ready(function () {
         event.preventDefault();
         $(".display").empty();
         createRow();
-        let = cityName = $("#cityname").val();
+        cityName = $("#cityname").val();
         let longtitude;
         let latitude;
-        let iconcode;
+
 
         if (cityName != "") {
 
             $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric" + "&APPID=45bb91d664f0e678d9a99e88e2efe20e",
+                url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric" + "&appid=45bb91d664f0e678d9a99e88e2efe20e",
                 type: "GET",
                 dataType: "jsonp",
-                success: function(response) {
-                    cityNameDisplay = response.name;
-                    iconcode = response.weather[0].icon;
+                success: function (response) {
+                    let iconcode = response.weather[0].icon;
+                    let iconlink = "https://openweathermap.org/img/w/" + iconcode + ".png";
+                    $(".citynamedisplay").text(response.name + " " + convertUnix(response.dt));
+                    $('#wicon').attr('src', iconlink);
                     $(".temp").text(response.main.temp + " °C");
                     $(".humidity").text(response.main.humidity + " %");
-                    $(".wind").text(response.wind.speed + " KPH");
+                    $(".wind").text(response.wind.speed + " KM/H");
                     $(".uvindex").text(response.wind.speed + " UV Index");
                     longtitude = response.coord.lon;
                     latitude = response.coord.lat;
 
-                // display icon of appropriate weather
-                let iconlink = "https://openweathermap.org/img/w/" + iconcode + ".png";
-                // display uv index using longtitude and latitude
-                let secondqueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=45bb91d664f0e678d9a99e88e2efe20e&lat=" + latitude + "&lon=" + longtitude;
-                $.ajax({
-                    url: secondqueryURL,
-                    method: "GET"
-                }).then(function (response) {
-                    $(".uvindex").text(response.value);
-                    $(".uvindex").css("background-color", "crimson");
-                    $(".cityNameDisplay").text(cityNameDisplay + " " + "(" + response.date_iso + ")");
-                    $('#wicon').attr('src', iconlink);
-                    $("#wicon").css("display", "block");
-                });
-            }});
+                    let secondIcon;
+                    // display weather forecast of 5 days
+                    let thirdqueryURL = "https://api.openweathermap.org/data/2.5/onecall?appid=45bb91d664f0e678d9a99e88e2efe20e&lat=" + latitude + "&lon=" + longtitude;
+                    $.ajax({
+                        url: thirdqueryURL,
+                        method: "GET"
+                    }).then(function (response) {
+
+                        let fiveDayWeather = response.daily.slice(-5);
+                        for (let i = 0; i < fiveDayWeather.length; i = i + 1) {
+                            let newDiv = $("<div>");
+                            newDiv.addClass("col forecast");
+                            let date = $("<h3>").text("Date: "+ fiveDayWeather[i].dt_txt);
+                            secondIcon = fiveDayWeather[i].weather[0].icon;
+                            let secondIconlink = "https://openweathermap.org/img/w/" + secondIcon + ".png";
+                            let icon = $("<img>").attr('src', secondIconlink);
+                            let temp = $("<p>").text("Temperature: " + (fiveDayWeather[i].temp.day - 273.15).toFixed(2) + " °C");
+                            let wind = $("<p>").text("Wind Speed: " + fiveDayWeather[i].wind_speed + " KM/H");
+                            let humidity = $("<p>").text("Humidity: " + fiveDayWeather[i].humidity + " %");
+                            let uvindex = $("<p>").text("UV Index: " + fiveDayWeather[i].uvi);
+
+
+                            newDiv.append(date, icon, temp, humidity, wind, uvindex);
+                            $(".display").append(newDiv);
+                        };
+                    });
+
+                    // display icon of appropriate weather
+
+                    // display uv index using longtitude and latitude
+                    let secondqueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=45bb91d664f0e678d9a99e88e2efe20e&lat=" + latitude + "&lon=" + longtitude;
+                    $.ajax({
+                        url: secondqueryURL,
+                        method: "GET",
+                    }).then(function (response) {
+                        $(".uvindex").text(response.value);
+                        $(".uvindex").css("background-color", "crimson");
+                        // $(".citynamedisplay").text(cityNameDisplay + " " + "(" + response.date_iso + ")");
+
+                        $("#wicon").css("display", "block");
+                    });
+                }
+            });
             // If user does not put a city name in input area
         } else {
             $("#error").html("Field cannot be empty");
         }
 
-                    let secondIcon;
-                    // display weather forecast of 5 days
-                    let thirdqueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=metric" + "&APPID=45bb91d664f0e678d9a99e88e2efe20e";
-                    $.ajax({
-                        url: thirdqueryURL,
-                        method: "GET"
-                    }).then(function (response) {
-        
-                        let fiveDayWeather = response.list;
-                        for (let i = 0; i < fiveDayWeather.length; i = i + 8) {
-                            let newDiv = $("<div>");
-                            newDiv.addClass("col forecast");
-                            secondIcon = fiveDayWeather[i].weather[0].icon;
-                            let secondIconlink = "https://openweathermap.org/img/w/" + secondIcon + ".png";
-                            let date = $("<h3>").text(fiveDayWeather[i].dt_txt);
-                            let icon = $("<img>").attr('src', secondIconlink);
-                            let temp = $("<p>").text("Temperature: " + fiveDayWeather[i].main.temp + " °C");
-                            let humidity = $("<p>").text("Humidity: " + fiveDayWeather[i].main.humidity + " %");
-        
-                            newDiv.append(date, icon, temp, humidity);
-                            $(".display").append(newDiv);
-                        };
-                    });
-        
-                });
-            });
+    });
+});
 
-    // Create a list of locations that have been searched
-    let createRow = function () {
-        let = cityName = $("#cityname").val();
-        let button = $("<button>").text(cityName);
-        button.addClass("locationname list-group-item list-group-item-action");
-        button.attr("type", "button");
-        $(".location-list").prepend(button);
-    };
+// Create a list of locations that have been searched
+let createRow = function () {
+    let cityName = $("#cityname").val();
+    let button = $("<button>").text(cityName);
+    button.addClass("locationname list-group-item list-group-item-action");
+    button.attr("type", "button");
+    $(".location-list").prepend(button);
+};
 
 
+
+    // click on a city in the search history & take user back to city information
+  
+
+  
+
+    //clear search history
     
